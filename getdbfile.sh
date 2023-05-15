b1b6b43b78c6bf75dd8a5a0277f15285e8b5586a
kitsdk <kitsdk@163.com>
2023-05-15 12:04:24 +0800

    更新脚本注释

--- getdbfile.sh
+++ getdbfile.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 # 从nv scanner中提取cvddb文件
 
+# 检查scanner容器镜像是否存在
 scanner=`docker images neuvector/scanner:latest |wc -l`
 if [ $scanner -lt 2 ]; then 
-
   echo "没有发现 neuvector/scanner:latest镜像,请先pull镜像文件"
+  echo "执行命令docker pull neuvector/scanner 可pull镜像"
   exit 
-
 fi
 
 if [ -f "/tmp/nvdbtools/cvedb" ];then
   rm  /tmp/nvdbtools/cvedb
 fi
+#检查 scanner容器是否处于运行状态
 ID=`docker ps -a  |grep cvedb |grep Created |awk '{print $1}'`
 if [  -n "$ID" ]; then
  docker rm -f $ID
 fi
-
+#运行scanner容器
 docker container create --name cvedb neuvector/scanner
-
+#获取容器ID
 ID=`docker ps -a  |grep cvedb |awk '{print $1}'`
-
 
 if [ ! -n "$ID" ]; then
     echo "scanner container is not created"
 else
+#copy cvedb数据库文件 到本地
     docker cp   ${ID}":/etc/neuvector/db/cvedb" /tmp/nvdbtools/
 fi
+# 删除运行的容器
 docker rm -f $ID
-#docker cp   ${ID}":/etc/neuvector/db/cvedb" /tmp/ 
+
 
 
