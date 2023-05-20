0dd4b4204f76c7b1782f7adb3aa86a1c4a06d202
kitsdk <kitsdk@163.com>
2023-05-20 22:17:37 +0800

    更新同步下载

--- cmd/cnnvd_download.go
+++ cmd/cnnvd_download.go
@@ -6,6 +6,7 @@ package cmd
 import (
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -20,11 +21,26 @@ var downloadCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		// 获取token参数
 		token, err := cmd.Flags().GetString("token")
-		if err != nil {
+		if err != nil || token == "" {
 			log.Println("请输入正确token:", err)
 			return
 
 		}
+		// xml文件保存路径
+		savePath, err := cmd.Flags().GetString("savePath")
+		if err != nil {
+			log.Println("请输入正确filePath:", err)
+			return
+		}
+
+		if err != nil {
+			log.Println("输入正确下载超时参数（秒）", err)
+			return
+		}
+		// 初始化路径
+		os.RemoveAll(savePath)
+		os.MkdirAll(savePath, 0755)
+
 		// 获取cve文件的ID列表
 		list, err := cnnvd.GetIDlist()
 		if err != nil {
@@ -35,10 +51,10 @@ var downloadCmd = &cobra.Command{
 		ch := make(chan string)
 		for _, id := range list {
 
-			go func(fid, token string) {
+			go func(fid, token, spath string) {
 				// 下载xml文件
-				ch <- cnnvd.GetXml(fid, token)
-			}(id, token)
+				ch <- cnnvd.GetXml(fid, token, spath)
+			}(id, token, savePath)
 
 		}
 		timeout := time.After(900 * time.Second)
@@ -46,9 +62,9 @@ var downloadCmd = &cobra.Command{
 			select {
 			case res := <-ch:
 				nt := time.Now().Format("2006-01-02 15:04:05")
-				fmt.Printf("[%s]完成下载： %s\n", nt, res)
+				fmt.Printf("[%s]完成下载：%s%s.xml\n", nt, savePath, res)
 			case <-timeout:
-				fmt.Println("超时...")
+				fmt.Println("超时...:", savePath+".xml")
 				break
 			}
 		}
@@ -58,8 +74,9 @@ var downloadCmd = &cobra.Command{
 }
 
 func init() {
-	cnnvdCmd.AddCommand(downloadCmd)
-	cnnvdCmd.Flags().StringP("token", "t", "", "cnnvd官网登陆后获取的token字符串")
+	rootCmd.AddCommand(downloadCmd)
+	downloadCmd.Flags().StringP("token", "t", "", "cnnvd官网登陆后获取的token字符串")
+	downloadCmd.Flags().StringP("savePath", "s", "/tmp/nvdbtools/xml/", "cnnvd xml数据库的保存目录")
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
