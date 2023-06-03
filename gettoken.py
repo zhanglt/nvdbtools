2b82658102294c5f3fa4ee684ab36e9943202ad4
kitsdk <kitsdk@163.com>
2023-06-03 20:23:15 +0800

    优化脚本

--- gettoken.py
+++ gettoken.py
@@ -3,31 +3,39 @@
 import json
 import ddddocr
 import requests
+def gettoken():
 # cnncvd官网登陆认证地址
-urlLogin = "https://www.cnnvd.org.cn/web/login"
+    urlLogin = "https://www.cnnvd.org.cn/web/login"
 # cnnvd官网获取登录验证码图片地址
-urlImage = "https://www.cnnvd.org.cn/web/verificationCode/getBase64Image"
+    urlImage = "https://www.cnnvd.org.cn/web/verificationCode/getBase64Image"
 # 获取验证码图片（base64）
-response = requests.get(url=urlImage)
+    response = requests.get(url=urlImage)
 # json序列化
-imageData = json.loads(response.text)
+    imageData = json.loads(response.text)
 # 提取图片数据
-image =imageData["data"]["image"][22:]
-ocr = ddddocr.DdddOcr()
+    image =imageData["data"]["image"][22:]
+#print(image)
+    ocr = ddddocr.DdddOcr()
 # 图片识别
-code = ocr.classification(image)
+    code = ocr.classification(image)
+    #print(code)
 # 提取辅助验证信息
-verifyToken =imageData["data"]["verifyToken"]
+    verifyToken =imageData["data"]["verifyToken"]
+    #print(verifyToken)
 # 登录信息
-postData ={
-    "username": "",
-    "password": "",
-    "code": code,
-    "verifyToken": verifyToken
-    }
+    postData ={
+        "username": "kitsdk@163.com",
+        "password": "bed128365216c019988915ed3add75fb",
+        "code": code,
+        "verifyToken": verifyToken
+        }
 # 模拟登录
-rq=requests.post(url=urlLogin,data=json.dumps(postData),headers={'Content-Type':'application/json'})
+    rq=requests.post(url=urlLogin,data=json.dumps(postData),headers={'Content-Type':'application/json'})
 # 序列化返回信息
-loginData = json.loads(rq.text)
+    loginData = json.loads(rq.text)
 #提取token
-print(loginData["data"]["token"])
+    print(loginData["data"]["token"])
+    #return (loginData["data"]["token"])
+
+if __name__ == '__main__' :
+    gettoken()
